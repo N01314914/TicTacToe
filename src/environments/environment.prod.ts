d9d4213749eff0006a43bfb64b8237b83b2ec59c
Manpreet kaur <Manpreet kaur@LAPTOP-9UK75AAO>
2019-06-09 13:11:26 -0400

    Complete App with single and multi user gameplay

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyCs-EzSXlXzFs8xq3uGJy2B3kyN7VVivS4",
+		authDomain: "tictactoe-563f2.firebaseapp.com",
+		databaseURL: "https://tictactoe-563f2.firebaseio.com",
+		projectId: "tictactoe-563f2",
+		storageBucket: "tictactoe-563f2.appspot.com",
+		messagingSenderId: "804138848130",
+		appId: "1:804138848130:web:2e7a5db9e2748423"
+	}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyCs-EzSXlXzFs8xq3uGJy2B3kyN7VVivS4",
+		authDomain: "tictactoe-563f2.firebaseapp.com",
+		databaseURL: "https://tictactoe-563f2.firebaseio.com",
+		projectId: "tictactoe-563f2",
+		storageBucket: "tictactoe-563f2.appspot.com",
+		messagingSenderId: "804138848130",
+		appId: "1:804138848130:web:2e7a5db9e2748423"
+  	}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
